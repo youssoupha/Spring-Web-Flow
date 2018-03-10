eb0be715ba33dce59ee5238e76a9659bca9af9a6
Youssoupha Sambe <youssouph.sambe@gmail.com>
2018-03-10 22:25:01 +0100

    Initial commit

---
+++ src/main/java/com/youss/springwebflow/SpringWebFlowApplication.java
@@ -0,0 +1,13 @@
+package com.youss.springwebflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFlowApplication.class, args);
+	}
+
+}
